09cedaaa181f81063bfacdf043b06e7554411121
iritep <rpat.upwork@gmail.com>
2022-10-17 13:27:53 -0500

    fix: eslint issue and integrated the database

--- src/config.js
+++ src/config.js
@@ -1,5 +1,10 @@
 export default {
   DEBUG: process.env.NODE_ENV !== 'production',
   LOG_LEVEL: process.env.LOG_LEVEL || 'info',
-  PORT: process.env.POORT
+  PORT: process.env.POORT,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_NAME: process.env.DB_NAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_USER: process.env.DB_USER
 };
--- src/database/index.js
+++ src/database/index.js
@@ -1,1 +1,17 @@
-export default {};
+import { logHelper } from '@/helpers';
+import { knex, Table, transaction } from './common';
+
+let tracingEnabled = false;
+function enableTracing() {
+  if (!tracingEnabled) {
+    knex.on('query', ({ sql, bindings }) => logHelper.trace(sql, bindings));
+  }
+  tracingEnabled = true;
+}
+
+export default {
+  knex,
+  enableTracing,
+  transaction,
+  Table
+};
