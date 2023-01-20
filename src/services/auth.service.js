15c5537df35a847060e9241170e9682128fd53e7
Ritesh <rpat.upwork@gmail.com>
2023-01-20 13:55:21 -0600

    chore: refresh test

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -195,7 +195,7 @@ export const verifyPhone = async (token, res) => {
     })
     .where({ id: verification.id });
 
-  const accessToken = await securityHelper.genJwtToken(user.id, '8h');
+  const accessToken = await securityHelper.genJwtToken(user.id, '10s');
   const refreshToken = await securityHelper.genRefreshToken(user.id, '24h');
 
   if (!config.DEBUG) securityHelper.setTokenToCookie(res, refreshToken);
