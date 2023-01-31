4242f31f04f89ffa5fd59c476acdaa9e58c2bb8b
Ritesh <rpat.upwork@gmail.com>
2023-01-31 13:29:05 -0600

    fix: points query

--- src/controller/point.controller.js
+++ src/controller/point.controller.js
@@ -2,7 +2,7 @@ import { pointService } from '@/services';
 
 export const getPoint = async (req, res) => {
   try {
-    const { userId, locationId } = req.params;
+    const { userId, locationId } = req.query;
     const result = await pointService.getPoint(userId, locationId);
     res.status(200).json(result);
   } catch (e) {
@@ -12,7 +12,7 @@ export const getPoint = async (req, res) => {
 
 export const getPoints = async (req, res) => {
   try {
-    const { userId } = req.params;
+    const { userId } = req.query;
     const result = await pointService.getPoints(userId);
     res.status(200).json(result);
   } catch (e) {
