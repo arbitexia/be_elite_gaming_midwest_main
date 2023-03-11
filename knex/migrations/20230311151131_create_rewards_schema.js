bec81865aa91810080a384b1a31f1491d16059e2
Federico <federicodido525@gmail.com>
2023-03-11 07:24:20 -0800

    create reward table

---
+++ knex/migrations/20230311151131_create_rewards_schema.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('rewards', function (table) {
+    table.increments();
+    table.integer('location_id').references('locations.id').deferrable('deferred');
+    table.integer('product_id').references('products.id').deferrable('deferred');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('rewards');
+};
